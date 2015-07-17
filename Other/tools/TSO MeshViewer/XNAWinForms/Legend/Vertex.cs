b74a837be76b6f78a5ce29b494b7547aa404df97
Zack Casey <aidancheddar@gmail.com>
2015-07-17 12:30:46 -0400

    Final wave
    Should be the last of the license clean up.

--- Other/tools/TSO MeshViewer/XNAWinForms/Legend/Vertex.cs
+++ Other/tools/TSO MeshViewer/XNAWinForms/Legend/Vertex.cs
@@ -1,13 +1,6 @@
 /*This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 If a copy of the MPL was not distributed with this file, You can obtain one at
-http://mozilla.org/MPL/2.0/.
-
-The Original Code is the TSO MeshViewer.
-
-The Initial Developer of the Original Code is
-Mats 'Afr0' Vederhus. All Rights Reserved.
-
-*/
+http://mozilla.org/MPL/2.0/.*/
 
 using System;
 using System.Collections.Generic;
