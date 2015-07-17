9e1e7a0d1f61908cd7ed9b8476a5da30a3332b5f
francot514 <francot514@hotmail.com>
2015-07-17 16:18:01 -0500

    Custom content loading

--- TSOClient/tso.content/WorldObjectProvider.cs
+++ TSOClient/tso.content/WorldObjectProvider.cs
@@ -10,6 +10,7 @@ using System.Text.RegularExpressions;
 using TSO.Files.formats.iff;
 using TSO.Files.formats.iff.chunks;
 using TSO.Files.formats.otf;
+using System.IO;
 
 namespace TSO.Content
 {
@@ -23,6 +24,7 @@ namespace TSO.Content
         private FAR1Provider<Iff> Sprites;
         private FAR1Provider<OTF> TuningTables;
         private Content ContentManager;
+        private List<Iff> Downloads;
 
         private Dictionary<ulong, GameObjectReference> Entries;
 
@@ -39,6 +41,7 @@ namespace TSO.Content
             Iffs = new FAR1Provider<Iff>(ContentManager, new IffCodec(), "objectdata\\objects\\objiff.far");
             Sprites = new FAR1Provider<Iff>(ContentManager, new IffCodec(), new Regex(".*\\\\objspf.*\\.far"));
             TuningTables = new FAR1Provider<OTF>(ContentManager, new OTFCodec(), new Regex(".*\\\\objotf.*\\.far"));
+            Downloads = new List<Iff>();
 
             Iffs.Init();
             TuningTables.Init();
@@ -61,6 +64,25 @@ namespace TSO.Content
                     FileName = objectInfo.Attributes["n"].Value
                 });
             }
+
+            DirectoryInfo info = new DirectoryInfo(this.ContentManager.GetPath(@"userdata\downloads"));
+            foreach (FileInfo info2 in info.GetFiles())
+            {
+                if (info2.Extension == "iff")
+                {
+                    this.Downloads.Add(new Iff(info2.FullName));
+                }
+            }
+            var document3 = new XmlDocument();
+            document3.Load(this.ContentManager.GetPath(@"packingslips\downloads.xml"));
+            XmlNodeList list3 = document3.GetElementsByTagName("I");
+            foreach (XmlNode node3 in list3)
+            {
+                ulong num;
+                num = Convert.ToUInt32(node3.Attributes["g"].Value, 0x10);
+                this.Entries.Add(num, new GameObjectReference(this) { ID = num, FileName = node3.Attributes["n"].Value });
+            }
+
         }
 
         private List<string> ProcessedFiles = new List<string>();
