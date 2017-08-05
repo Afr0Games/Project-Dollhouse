79fe103ddf7cb1f10ea2576fc5166dc30314c4fc
Mats Vederhus <afr0_at_afr0games.com>
2017-08-05 11:20:34 +0200

    Added Simantics project
    Started working on Simantics.

---
+++ TSOClient/SimAntics/ObjectModule.cs
@@ -0,0 +1,36 @@
+﻿namespace SimAntics
+{
+    public class ObjectModule
+    {
+        private Simulator m_Simulator;
+        private XPerson m_SelectedPerson;
+
+        public ObjectModule(Simulator Sim)
+        {
+            m_Simulator = Sim;
+        }
+
+        public XObject GetObjectByGUID()
+        {
+
+        }
+
+        public XPerson GetPersonByGUID()
+        {
+
+        }
+
+        public XPerson GetSelectedPerson()
+        {
+            return m_SelectedPerson;
+        }
+
+        /// <summary>
+        /// Updates all objects in the simulation by simulating a tick.
+        /// </summary>
+        public void UpdateSimObjects()
+        {
+
+        }
+    }
+}
