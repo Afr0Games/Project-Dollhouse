d44f7be2d2ec664d13ef7132af375ddcdd1fb18d
Mats Vederhus <afr0_at_afr0games.com>
2020-09-29 00:06:59 +0200

    Updated CityRenderer

---
+++ Other/tools/CityRenderer/CityRenderer/QuadTreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace Cityrenderer
+{
+    /// <summary>
+    /// This class represents the root and branches of the terrain's quadtree.
+    /// </summary>
+    public class QuadTreeNode
+    {
+        public BoundingBox boundingBox;
+        public QuadTreeNode topLeft, topRight, bottomLeft, bottomRight;
+
+        public QuadTreeNode(BoundingBox boundingBox)
+        {
+            this.boundingBox = boundingBox;
+        }
+    }
+}
